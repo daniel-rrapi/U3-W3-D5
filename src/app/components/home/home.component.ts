7ca17cd2ceeb002946e87684572d0ae05b05161d
Daniel Rrapi <uzo1209@gmail.com>
2023-12-23 12:13:04 +0100

    fixed home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/service/movies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private mvsSrv: MoviesService) {}
+  movieCarousel1: string = '';
+  movieCarousel2: string = '';
+  movieCarousel3: string = '';
   ngOnInit(): void {
+    this.mvsSrv.getMovies().subscribe((res) => {
+      console.log(res);
+      this.movieCarousel1 = res[0].poster_path;
+      this.movieCarousel2 = res[1].poster_path;
+      this.movieCarousel3 = res[2].poster_path;
+    });
   }
-
 }
